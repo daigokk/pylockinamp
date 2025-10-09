65d33e3c8adcbbd008dce773c98eb592deda36c8
daigokk <88434718+daigokk@users.noreply.github.com>
2025-10-09 10:56:19 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylia",
+    version="2.0.0",
+    description="Python wrapper of LIA",
+    author="daigokk",
+    packages=find_packages(),
+    install_requires=[],
+)
