849cdbc2c1a209be55234b411c2774f7eebcd23b
daigokk <88434718+daigokk@users.noreply.github.com>
2025-10-09 17:56:29 +0900

    Update pylia.py

--- pylia/pylia.py
+++ pylia/pylia.py
@@ -5,61 +5,172 @@ import numpy as np
 
 
 class pylia:
-  def __init__(self, option=''):
-    exe_path = os.path.join(os.path.dirname(__file__), 'bin', 'lia.exe')
-    self.process = subprocess.Popen(
-        f'{exe_path} pipe {option}',
-          stdin=subprocess.PIPE,
-          stdout=subprocess.PIPE,
-          encoding='utf-8',
-    )
-    print(self._recieve())
-  def __del__(self):
-    self._send('end')
-    time.sleep(1)
-    self.process.kill()
-  def _send(self, cmd:str):
-    self.process.stdin.write(f'{cmd}\n')
-    self.process.stdin.flush()
-  def _recieve(self, ):
-    self.process.stdout.flush()
-    return self.process.stdout.readline()[:-1]
-  def _query(self, cmd):
-    self._send(cmd)
-    return self._recieve()
-  def get_help(self):
-    size = int(self._query('help:size?'))
-    self._send('help?')
-    for i in range(size):
-      print(self._recieve())
-    return
-  def get_idn(self):
-    return self._query('*idn?')
-  def set_reset(self):
-    self._send('*rst')
-    return
-  def set_pause(self):
-    self._send('pause')
-    return
-  def set_run(self):
-    self._send('run')
-    return
-  def get_raw(self):
-    dat = []
-    size = int(self._query(':data:raw:size?'))
-    self._send(':data:raw?')
-    for i in range(size):
-      dat.append(list(map(float,self._recieve().split(','))))
-    return np.array(dat)
-  def get_xy(self):
-    return list(map(float,self._query(':data:xy?').split(',')))
-  def get_txy(self, sec=0):
-    dat = []
-    size = int(self._query(f':data:txy? {sec}'))
-    for i in range(size):
-      dat.append(list(map(float,self._recieve().split(','))))
-    return np.array(dat)
-  def get_fgFreq(self):
-    return float(self._query(':w1:freq?'))
-  def set_fgFreq(self, freq):
-    self._send(f':w1:freq {freq}\n')
+    """
+    Python wrapper for the LIA executable.
+    LIA: https://github.com/daigokk/LIA/tree/master
+    This class provides an interface to communicate with the LIA binary via subprocess piping.
+    It allows sending commands, receiving responses, and retrieving measurement data.
+    """
+
+    def __init__(self, option=''):
+        """
+        Initialize the LIA subprocess with optional command-line arguments.
+
+        Args:
+            option (str): When the option is 'nogui', Headless mode is used.
+        """
+        exe_path = os.path.join(os.path.dirname(__file__), 'bin', 'lia.exe')
+        self.process = subprocess.Popen(
+            f'{exe_path} pipe {option}',
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            encoding='utf-8',
+        )
+        print(self._recieve())
+
+    def __del__(self):
+        """
+        Destructor to safely terminate the LIA subprocess.
+        Sends 'end' command and kills the process after a short delay.
+        """
+        self._send('end')
+        time.sleep(1)
+        self.process.kill()
+
+    def _send(self, cmd: str):
+        """
+        Send a command string to the LIA process.
+
+        Args:
+            cmd (str): Command to send.
+        """
+        self.process.stdin.write(f'{cmd}\n')
+        self.process.stdin.flush()
+
+    def _recieve(self):
+        """
+        Receive a single line of response from the LIA process.
+
+        Returns:
+            str: Response string without trailing newline.
+        """
+        self.process.stdout.flush()
+        return self.process.stdout.readline()[:-1]
+
+    def _query(self, cmd):
+        """
+        Send a command and wait for the response.
+
+        Args:
+            cmd (str): Command to send.
+
+        Returns:
+            str: Response from the LIA process.
+        """
+        self._send(cmd)
+        return self._recieve()
+
+    def get_help(self):
+        """
+        Print help information from the LIA process.
+
+        Returns:
+            None
+        """
+        size = int(self._query('help:size?'))
+        self._send('help?')
+        for i in range(size):
+            print(self._recieve())
+
+    def get_idn(self):
+        """
+        Query the device identification string.
+
+        Returns:
+            str: Identification string.
+        """
+        return self._query('*idn?')
+
+    def set_reset(self):
+        """
+        Send reset command to the device.
+
+        Returns:
+            None
+        """
+        self._send('*rst')
+
+    def set_pause(self):
+        """
+        Pause the device operation.
+
+        Returns:
+            None
+        """
+        self._send('pause')
+
+    def set_run(self):
+        """
+        Resume or start the device operation.
+
+        Returns:
+            None
+        """
+        self._send('run')
+
+    def get_raw(self):
+        """
+        Retrieve raw data from the device.
+
+        Returns:
+            np.ndarray: 2D array of float values.
+        """
+        dat = []
+        size = int(self._query(':data:raw:size?'))
+        self._send(':data:raw?')
+        for i in range(size):
+            dat.append(list(map(float, self._recieve().split(','))))
+        return np.array(dat)
+
+    def get_xy(self):
+        """
+        Retrieve XY data from the device.
+
+        Returns:
+            list: List of two or four float values [X, Y].
+        """
+        return list(map(float, self._query(':data:xy?').split(',')))
+
+    def get_txy(self, sec=0):
+        """
+        Retrieve time-series XY data for a given duration.
+
+        Args:
+            sec (int): Duration in seconds to collect data.
+
+        Returns:
+            np.ndarray: 3D or 5D array of float values.
+        """
+        dat = []
+        size = int(self._query(f':data:txy? {sec}'))
+        for i in range(size):
+            dat.append(list(map(float, self._recieve().split(','))))
+        return np.array(dat)
+
+    def get_fgFreq(self):
+        """
+        Get the frequency of the function generator 'W1'.
+
+        Returns:
+            float: Frequency value.
+        """
+        return float(self._query(':w1:freq?'))
+
+    def set_fgFreq(self, freq):
+        """
+        Set the frequency of the function generator 'W1'.
+
+        Args:
+            freq (float): Frequency value to set.
+        """
+        self._send(f':w1:freq {freq}\n'
