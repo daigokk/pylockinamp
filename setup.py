28667f1f666c1f544d62fb065418f05900ad6e7a
daigokk <88434718+daigokk@users.noreply.github.com>
2025-10-09 13:20:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ setup(
     name='pylia',
     version='2.0.0',
     description='Python wrapper of LIA',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     author='daigokk',
+    url='https://github.com/daigokk/pylia',
     packages=find_packages(),
-    platforms=['Windows'],
     include_package_data=True,  # MANIFEST.in に従って追加ファイルを含める
     package_data={
-        'pylia': ['bin/lia.exe'],  # バイナリを含める
+        'pylia': ['pylia/bin/lia.exe'],  # バイナリを含める
     },
     classifiers=[
         'Programming Language :: Python :: 3',
