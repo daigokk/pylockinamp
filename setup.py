79b724e63e610bd0b2ba264ba31c3e6ea5979dd1
daigokk <88434718+daigokk@users.noreply.github.com>
2025-10-09 11:04:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pylia",
-    version="2.0.0",
-    description="Python wrapper of LIA",
-    author="daigokk",
+    name='pylia',
+    version='2.0.0',
+    description='Python wrapper of LIA',
+    author='daigokk',
     packages=['windows_only_package'],
     platforms=['Windows'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
     ],
     install_requires=[
         numpy,
